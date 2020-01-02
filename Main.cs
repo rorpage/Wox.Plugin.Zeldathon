6ed787972d10aa1832be521a484d8ba575d8f5a7
Robbie Page <robbie.page@e-gineering.com>
2020-01-02 08:29:49 -0500

    initial commit

---
+++ Main.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace Wox.Plugin.Zeldathon
+{
+    public class Main : IPlugin
+    {
+        public void Init(PluginInitContext context) { }
+        public List<Result> Query(Query query)
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls
+                                                   | SecurityProtocolType.Tls11
+                                                   | SecurityProtocolType.Tls12
+                                                   | SecurityProtocolType.Ssl3;
+
+            var currentGame = GetCurrentGame();
+            var donationTotal = GetDonationTotal();
+
+            return new List<Result>
+            {
+                new Result
+                {
+                    IcoPath = "Images\\zeldathon.png",
+                    SubTitle = $"{currentGame}",
+                    Title = "Current game",
+                    Action = _ => true
+                },
+                new Result
+                {
+                    IcoPath = "Images\\zeldathon.png",
+                    SubTitle = $"{donationTotal}",
+                    Title = "Donation total",
+                    Action = _ => true
+                }
+            };
+        }
+
+        private static string GetDonationTotal()
+        {
+            using (var webClient = new WebClient())
+            {
+                var response = webClient.DownloadString("https://zeldathon.net/api/Kinstone/total");
+                var responseArray = JArray.Parse(response);
+                return (string) responseArray[0];
+            }
+        }
+
+        private static string GetCurrentGame()
+        {
+            using (var webClient = new WebClient())
+            {
+                webClient.Headers.Add("client-id", "jzkbprff40iqj646a697cyrvl0zt2m6");
+
+                var response = webClient.DownloadString("https://api.twitch.tv/kraken/channels/supermcgamer");
+                var jsonObject = JObject.Parse(response);
+
+                return (string) jsonObject["game"];
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Wox.Plugin.Zeldathon
+A Wox plugin to retrieve the current game and donation total from Zeldathon
